f31d6fe3bcaa7bdf8c2b5e591f3848fd9cbb4da0
noname <mbuczek111@gmail.com>
2020-04-27 20:43:18 +0200

    Initial commit

---
+++ src/LetterCounterNew.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class LetterCounterNew {
+
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a phrase: ");
+        String phrase = input.nextLine();
+
+        phrase = phrase.replaceAll("\\s+","");
+
+        TreeMap<Character, Integer> map = new TreeMap<Character, Integer>();
+        for (int i = 0; i < phrase.length(); i++) {
+            char letter = phrase.charAt(i);
+            Integer counter = map.get(letter);
+            if (counter != null) {
+                map.put(letter, counter + 1);
+            }
+            else {
+                map.put(letter, 1);
+            }
+        }
+        for (int i = 0; i < phrase.length(); i++) {
+            System.out.print(phrase.charAt(i) + "-" + map.get(phrase.charAt(i))+ ", ");
+        }
+    }
+
+}
